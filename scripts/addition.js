faefef7d3e8cd07d7077bf569e45e95e58977033
John Micko <jrmicko5012@gmail.com>
2020-11-03 17:23:50 -0600

    addition checkpoint complete

--- scripts/addition.js
+++ scripts/addition.js
@@ -1,1 +1,11 @@
 console.log('js');
+// take two number variables
+function addition(num1, num2){
+    // add the two variables and return
+    return (num1 + num2);
+}
+
+// test
+console.log('testing addition');
+console.log('should log 5:', addition(2, 3));
+console.log('should log 9:', addition(2, 7));
