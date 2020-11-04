91cf561cab00ab0dd78ca446b2fa71d2b60ee485
John Micko <jrmicko5012@gmail.com>
2020-11-03 19:40:31 -0600

    array of objects stretch goal complete

--- scripts/LoopsArrayOfObjects.js
+++ scripts/LoopsArrayOfObjects.js
@@ -1,1 +1,24 @@
 console.log('LoopsArrayOfObjects js');
+
+function totalYearsOfExperience(company){
+    // start a variable to hold the sum value
+    let totalYears = 0;
+    // dig into the first level of the array, look at each object 1 at a time
+    for (const employee of company) {
+        // add the years tp to the totalYears sum
+        totalYears += employee.yearsOfExperience;
+    }
+    // return the sum
+    return totalYears;
+}
+
+// test
+console.log('should log 14', totalYearsOfExperience([
+    { name: "Tony", yearsOfExperience: 10 }, 
+    { name: "Carla", yearsOfExperience: 4 }
+]));
+console.log('should log 28', totalYearsOfExperience([
+    { name: "Tony", yearsOfExperience: 10 }, 
+    { name: "Carla", yearsOfExperience: 4 },
+    { name: "Kris", yearsOfExperience: 14 }
+]));
